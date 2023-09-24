04aee788d89ca422c3718f420483862579a6ae64
SpookedSymphony <s222286197@deakin.edu.au>
2023-09-25 00:06:54 +1000

    Initial commit

---
+++ src/components/Footer.vue
@@ -0,0 +1,14 @@
+<style scoped>
+footer {
+  padding: 10px;
+  text-align: center;
+  color: white;
+}
+</style>
+<template>
+    <footer>
+        <h4>© 2023 Business Is Us</h4>
+        <h4>Email: Businessisus@Businessisus.gov.au</h4>
+        <h4>Phone: 0378521634</h4>
+    </footer>
+</template>
---
+++ src/views/AboutView.vue
@@ -0,0 +1,33 @@
+<script setup>
+  import Footer from '../components/Footer.vue'
+  import '@/assets/column.css';
+</script>
+
+<template>
+  <main>
+    <div class="row">
+      <div class="column">
+        <img src='@/assets/man.jpg' alt="man" style="width:200px;height:150px;">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat urna eget tristique. Maecenas sagittis massa id ante dignissim, ac interdum odio pellentesque. Nullam et enim in tortor varius interdum.</p>
+      </div>
+      <div class="column">
+        <img src='@/assets/woman.jpg' alt="woman" style="width:200px;height:150px;">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed consequat urna eget tristique. Maecenas sagittis massa id ante dignissim, ac interdum odio pellentesque. Nullam et enim in tortor varius interdum.</p>
+
+
+
+      </div>
+    </div>
+    <Footer/>
+  </main>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
